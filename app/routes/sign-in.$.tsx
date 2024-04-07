60d42a7fc457377910d3086b4ad10e611696c4ce
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-04-07 20:05:47 +0300

    add paths to sign in/sign up
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -4,7 +4,7 @@ export default function SignInPage() {
   return (
     <div className='w-[100vw] min-h-[100vh] place-items-center'>
       <h1>Sign In route</h1>
-      <SignIn />
+      <SignIn path="/sign-in"/>
     </div>
   )
 }
