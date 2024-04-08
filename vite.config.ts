2123994c283359f29962eef851d8a577fe08d7cf
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-04-09 01:36:03 +0300

    attempt vite dependencies 504 fix
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,33 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { installGlobals } from '@remix-run/node'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-installGlobals();
+installGlobals()
 
 export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
   server: {
-    host:true,
-    port: 3000
+    host: true,
+    port: 3000,
   },
   preview: {
-    port: 3000
-  }
-});
+    port: 3000,
+  },
+
+  // NOTE optimizeDeps may be overkill, but ometimes throws 504
+  // @see https://github.com/remix-run/remix/issues/8803
+  optimizeDeps: {
+    entries:[
+      './app/**/*.{tsx}',
+    ],
+    include: [
+      '@remix-run/react',
+      '@prisma/client',
+      '@zenstackhq/runtime',
+      '@conform-to/zod',
+      '@conform-to/react',
+      'zod',
+    ],
+  },
+})
