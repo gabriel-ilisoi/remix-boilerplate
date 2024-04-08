2ce4e18a7e5d5c93df8ed551914c1a9cff2d2840
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-04-08 12:46:52 +0300

    add clearBrowserExtensionInjectionsBeforeHydration
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,7 +8,46 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
+function clearBrowserExtensionInjectionsBeforeHydration() {
+  document
+    .querySelectorAll(
+      [
+        'html > *:not(body, head)',
+        'script[src*="extension://"]',
+        'link[href*="extension://"]',
+      ].join(', ')
+    )
+    .forEach((s) => {
+      s.parentNode?.removeChild(s)
+    })
+
+  const $targets = {
+    html: {
+      $elm: document.querySelector('html')!,
+      allowedAttributes: ['lang', 'dir', 'class'],
+    },
+    head: {
+      $elm: document.querySelector('head')!,
+      allowedAttributes: [''],
+    },
+    body: {
+      $elm: document.querySelector('body')!,
+      allowedAttributes: ['class'],
+    },
+  }
+
+  Object.entries($targets).forEach(([, target]) => {
+    target.$elm.getAttributeNames().forEach((attr) => {
+      if (!target.allowedAttributes.includes(attr)) {
+        target.$elm.removeAttribute(attr)
+      }
+    })
+  })
+}
+
+
 startTransition(() => {
+  clearBrowserExtensionInjectionsBeforeHydration()
   hydrateRoot(
     document,
     <StrictMode>
