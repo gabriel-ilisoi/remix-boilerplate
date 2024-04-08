e39b64189ce0a9a8277c56b0c289e3fd164de51c
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-04-09 02:08:34 +0300

    cleanup;  stable
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/remix'
 
 export default function SignInPage() {
   return (
-    <div className='flex flex-col place-items-center'>
+    <div className='flex flex-col place-items-center py-8'>
         {/* <h1 className='mb-8'>Sign In route</h1> */}
         <SignIn path="/sign-in"/>
     </div>
